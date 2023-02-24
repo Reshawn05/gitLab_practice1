ae302f8b9e24a388c5497f8ef1f80656407384fa
azverndias09 <azverndias09@gmail.com>
2023-02-24 14:41:09 +0530

    hii

---
+++ gitLab_practice1/src/gitLab_practice1/Orange.java
@@ -0,0 +1,20 @@
+package gitLab_practice1;
+
+public class Orange {
+	String color;
+	Orange(String color)
+	{
+		this.color=color;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Orange greenApple = new Orange ("Green");
+		String color = greenApple.getColor();
+		System.out.println("color of the apple is"+color);
+		
+		}
+
+	
+
+}
